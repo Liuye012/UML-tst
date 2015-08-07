a6bd44c2bff476ea2b2d59003db75ea4b710484c
jian <Jiandandian012@outlook.com>
2015-08-07 13:25:11 +0800

    form_serializer,form_serializer_spec finished

--- main/lib/form_serializer.js
+++ main/lib/form_serializer.js
@@ -1,31 +1,26 @@
 function formSerializer(form) {}
 
-function newResultItem(elem, result) {
-  var item = {};
-  item.name = elem.name;
-  item.type = elem.type;
-  item.value = [];
-  item.value.push(elem.value);
-  result.push(item);
-  return result;
-}
-
 formSerializer.prototype.getformSerializer = function(form) {
   var result = [];
   [].forEach.call(form, function(elem) {
-    if ((elem.type === "radio" && elem.checked === "checked") || (elem.type === "checkbox" && elem.checked === "checked")) {
+    if ((elem.type === "radio" && elem.checked === false) || (elem.type === "checkbox" && elem.checked === false)) {
 
     } else {
-      for (var i = 0,cnt=0; i < result.length; i++) {
+      var cnt = 0;
+      for (var i = 0; i < result.length; i++) {
         if (elem.name === result[i].name) {
           result[i].value.push(elem.value);
           cnt = 1;
         }
       }
       if (cnt === 0) {
-        newResultItem(elem, result);
+        var item = {};
+        item.name = elem.name;
+        item.type = elem.type;
+        item.value = [];
+        item.value.push(elem.value);
+        result.push(item);
       }
     }
   });
-
 };
--- spec/lib/form_serializer_spec.js
+++ spec/lib/form_serializer_spec.js
@@ -15,22 +15,12 @@ describe("formSerializer", function() {
       "<input name='Q2-1' type='radio' value='B' checked='checked' />" +
       "<input name='Q2-1' type='radio' value='C'  />" +
       "<input name='Q2-1' type='radio' value='D'  />" +
-      "<input name='Q2-2' type='radio' value='A'  />" +
-      "<input name='Q2-2' type='radio' value='B'  />" +
-      "<input name='Q2-2' type='radio' value='C' checked='checked'  />" +
-      "<input name='Q2-2' type='radio' value='D'  />" +
       "<input name='Q3-1' type='checkbox' value='A'  checked='checked' />" +
       "<input name='Q3-1' type='checkbox' value='B'  checked='checked' />" +
       "<input name='Q3-1' type='checkbox' value='C'  checked='checked' />" +
       "<input name='Q3-1' type='checkbox' value='D'  />" +
-      "<input name='Q3-2' type='checkbox' value='A'  checked='checked'/>" +
-      "<input name='Q3-2' type='checkbox' value='B'  checked='checked' />" +
-      "<input name='Q3-2' type='checkbox' value='C'  />" +
-      "<input name='Q3-2' type='checkbox' value='D'  checked='checked' />" +
       "<input name='Q4-1' type='radio' value='对' checked='checked' >" +
       "<input name='Q4-1' type='radio' value='错' >" +
-      "<input name='Q4-2' type='radio' value='对'  >" +
-      "<input name='Q4-2' type='radio' value='错' checked='checked'>" +
       "<textarea  name='Q5-1'rows='10' cols='100' >模型的表现形式</textarea>" +
       "</form>";
     form = document.getElementsByTagName('form');
@@ -50,27 +40,15 @@ describe("formSerializer", function() {
       name: "Q2-1",
       type: 'radio',
       value: ["B"]
-    }, {
-      name: "Q2-2",
-      type: 'radio',
-      value: ["C"]
-    }, {
+    },  {
       name: "Q3-1",
       type: 'checkbox',
       value: ["A", "B", "C"]
-    }, {
-      name: "Q3-2",
-      type: 'checkbox',
-      value: ["A", "B", "D"]
-    }, {
+    },{
       name: "Q4-1",
       type: 'radio',
       value: ["对"]
-    }, {
-      name: "Q4-2",
-      type: 'radio',
-      value: ["错"]
-    }, {
+    },  {
       name: "Q5-1",
       type: 'text',
       value: ['模型的表现形式']
